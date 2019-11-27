c69cd58888ae66843660cb968d49117c26529c6d
PsyKzz <matt.daemon660@gmail.com>
2019-11-27 23:44:19 +0000

    cachefrontpage

--- statbus/routes/frontpage.py
+++ statbus/routes/frontpage.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+
 from statbus.database import Round, PollQuestion
+from statbus.ext import cache
 
 bp = Blueprint("frontpage", __name__)
 
 
 @bp.route("/")
+@cache.cached()
 def index():
     active_polls = PollQuestion.get_active()
     recent_rounds = Round.get_recent()
