582a4af5e4c2d97812a88a1c73af256e6a7cdeb0
PsyKzz <matt.daemon660@gmail.com>
2019-11-27 00:37:10 +0000

    Testing

--- statbus/routes/frontpage.py
+++ statbus/routes/frontpage.py
@@ -7,7 +7,6 @@ bp = Blueprint("frontpage", __name__)
 @bp.route("/")
 def index():
     active_polls = PollQuestion.get_active()
-    print(active_polls)
     recent_rounds = Round.get_recent()
     return render_template(
         "index.html", recent_rounds=recent_rounds, active_polls=active_polls
--- tests/test_statbus.py
+++ tests/test_statbus.py
@@ -1,12 +1,13 @@
+import tempfile
 import pytest
-
 from statbus import create_app
 
 
 @pytest.fixture
 def client():
+    self.db_fd, self.db_name = tempfile.mkstemp()
     config = {}
-    config["DATABASE"] = {"name": "./testing.db", "engine": "peewee.SqliteDatabase"}
+    config["DATABASE"] = {"name": self.db_name, "engine": "peewee.SqliteDatabase"}
     config["TESTING"] = True
 
     app = create_app(config)
@@ -14,7 +15,13 @@ def client():
 
     yield client
 
-    # Cleanup if required
+    # Cleanup
+    os.close(self.db_fd)
+    os.unlink(self.db_name)
+
+
+def test_assert(self):
+    assert True
 
 
 def test_homepage(client):
